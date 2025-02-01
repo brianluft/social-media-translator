ad722a55ff650f220cc8cda6cbc6fddf83721780
Brian Luft <brian@electroly.com>
2025-02-01 01:12:03 -0400

    Swiftlint fix

--- TranslateVideoSubtitles/TranslateVideoSubtitlesUITests/TranslateVideoSubtitlesUITestsLaunchTests.swift
+++ TranslateVideoSubtitles/TranslateVideoSubtitlesUITests/TranslateVideoSubtitlesUITestsLaunchTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 
 final class TranslateVideoSubtitlesUITestsLaunchTests: XCTestCase {
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    override static var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
 
