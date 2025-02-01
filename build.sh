7f963f1696a3ac071f0cd9dfa7a10c80f6c3db40
Brian Luft <brian@electroly.com>
2025-02-01 01:11:53 -0400

    Add swiftlint

--- build.sh
+++ build.sh
@@ -2,41 +2,8 @@
 set -uo pipefail
 set +e
 
-echo "--- BuildTools Resolve ---"
-(cd BuildTools && NSUnbufferedIO=YES swift package resolve >/dev/null 2>/dev/null)
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-  echo "swift package resolve failed with exit code $EXIT_CODE"
-  exit 1
-fi
-
-echo "--- Build xcbeautify ---"
-(cd BuildTools && NSUnbufferedIO=YES swift build -q -c release --product xcbeautify >/dev/null 2>/dev/null)
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-  echo "swift build failed with exit code $EXIT_CODE"
-  exit 1
-fi
-
-XCBEAUTIFY=$(find BuildTools/.build -type f -name xcbeautify | grep -v dSYM)
-if [ -z "$XCBEAUTIFY" ]; then
-  echo "xcbeautify not found"
-  exit 1
-fi
-
-echo "--- Build SwiftFormat ---"
-(cd BuildTools && NSUnbufferedIO=YES swift build -q -c release --product swiftformat >/dev/null 2>/dev/null)
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-  echo "swift build failed with exit code $EXIT_CODE"
-  exit 1
-fi
-
-SWIFTFORMAT=$(find BuildTools/.build -type f -name swiftformat | grep apple-macosx | grep -v dSYM)
-if [ -z "$SWIFTFORMAT" ]; then
-  echo "swiftformat not found"
-  exit 1
-fi
+# Log the start time
+echo "=== Build started at $(date) ===" > log
 
 NUM_CORES=$(sysctl -n hw.ncpu)
 if [ -z "$NUM_CORES" ]; then
@@ -44,8 +11,44 @@ if [ -z "$NUM_CORES" ]; then
   exit 1
 fi
 
+echo "--- BuildTools Resolve ---"
+(cd BuildTools && NSUnbufferedIO=YES swift package resolve 2>&1) >> log
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "swift package resolve failed with exit code $EXIT_CODE"
+  exit 1
+fi
+
+echo "--- Build xcbeautify ---"
+(cd BuildTools && NSUnbufferedIO=YES swift build -q -c release --product xcbeautify 2>&1) >> log
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "swift build failed with exit code $EXIT_CODE"
+  exit 1
+fi
+
+XCBEAUTIFY=$(find BuildTools/.build -type f -name xcbeautify | grep -v dSYM | grep release)
+if [ -z "$XCBEAUTIFY" ]; then
+  echo "xcbeautify not found"
+  exit 1
+fi
+
+echo "--- Build SwiftFormat ---"
+(cd BuildTools && NSUnbufferedIO=YES swift build -q -c release --product swiftformat 2>&1) >> log
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "swift build failed with exit code $EXIT_CODE"
+  exit 1
+fi
+
+SWIFTFORMAT=$(find BuildTools/.build -type f -name swiftformat | grep apple-macosx | grep -v dSYM | grep release)
+if [ -z "$SWIFTFORMAT" ]; then
+  echo "swiftformat not found"
+  exit 1
+fi
+
 echo "--- Clean ---"
-NSUnbufferedIO=YES xcodebuild clean -quiet -workspace TranslateVideoSubtitles.xcworkspace -scheme TranslateVideoSubtitles -configuration Debug -destination 'generic/platform=iOS Simulator' 2>&1  | $XCBEAUTIFY --disable-logging
+NSUnbufferedIO=YES xcodebuild clean -quiet -workspace TranslateVideoSubtitles.xcworkspace -scheme TranslateVideoSubtitles -configuration Debug -destination 'generic/platform=iOS Simulator' 2>&1 | $XCBEAUTIFY --disable-logging
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   echo "xcodebuild clean failed with exit code $EXIT_CODE"
@@ -62,7 +65,7 @@ fi
 (cd VideoSubtitlesLib && rm -rf .build)
 
 echo "--- Format ---"
-$SWIFTFORMAT . >/dev/null 2>/dev/null
+($SWIFTFORMAT . 2>&1) >> log
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   echo "swiftformat failed with exit code $EXIT_CODE"
@@ -90,6 +93,14 @@ echo "--- Test ---"
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   echo "swift test failed with exit code $EXIT_CODE"
+  exit 1
+fi
+
+echo "--- Lint ---"
+swift package --package-path BuildTools --allow-writing-to-package-directory swiftlint --quiet --config ../.swiftlint.yml ../TranslateVideoSubtitles/TranslateVideoSubtitles/ ../VideoSubtitlesLib/Sources/
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "swiftlint failed with exit code $EXIT_CODE"
   exit 1
 fi
 
